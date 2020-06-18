1929056bd3fd57d6b6c0cc0e310b59eccd7e4933
MarcosPBrandao <brandaomarcosp@gmail.com>
2020-06-18 11:16:40 -0300

    Commit typescript Quando adicionar tipos

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import express, { request, response } from 'express';
-
+import { helloWorld } from './routes';
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({message: 'hello'});
-})
+app.get('/',helloWorld);
 
 app.listen(3333);
